1b34d65f9c8b5b3926f84bf34d393f8e5900d658
System <none>
2017-03-20 19:55:21 +0000

    🎻👡 Updated with Glitch

--- server.js
+++ server.js
@@ -11,17 +11,8 @@ http.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-
 //app.use(express.static(__dirname + '/node_modules')); 
 app.use(express.static('public'));
-
-/*
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-app.use(express.static('public'));
-*/
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -32,29 +23,34 @@ function addStock(symbol) {
   if (!stocksArr.includes(symbol))
   { 
     stocksArr.push(symbol);
-    io.sockets.emit('newstock', { symbol: symbol });
+    io.sockets.emit('newstock', { symbol: symbol });  // let all clients know
+  }
+}
+
+function delStock(symbol) {
+  for(var i = 0; i < stocksArr.length; i++) { 
+    if (stocksArr[i] === symbol) {
+      stocksArr.splice(i, 1);
+      io.sockets.emit('delstock', { symbol: symbol });  // let all clients know
+    }
   }
 }
 
 io.on('connection', function (socket) {
-  io.sockets.emit('broadcast message', { broadcast: 'msg' });
+  //io.sockets.emit('broadcast message', { broadcast: 'msg' });
   
   socket.emit('allstocks', { stocks: stocksArr });
-  
-  socket.emit('news', { hello: 'world' });
-  
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-  
-
-  
+    
   socket.on('newstock', function (data) {
     addStock(data.symbol);
     console.log("stocksArr=" + stocksArr);
   });
   
-  //socket.emit('newstock', { symbol: data.Elements[0].Symbol });
+  socket.on('delstock', function (data) {
+    delStock(data.symbol);
+    console.log("stocksArr=" + stocksArr);
+  });
+  
 });
 
 // listen for requests :)
