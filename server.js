6d20e8768966144addf9e3a1090ae1698df7b51e
System <none>
2017-03-20 19:15:42 +0000

    🐿✈️ Updated with Glitch

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var http = require('http').Server(app);
 var socketIo = require('socket.io');
 var io = socketIo(http);
 
-var port = process.env.PORT || 3000;
+var stocksArr = [];
 
+var port = process.env.PORT || 3000;
 http.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
@@ -26,14 +27,34 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+function addStock(symbol) {
+  // only add if stock is not already in list.
+  if (!stocksArr.includes(symbol))
+  { 
+    stocksArr.push(symbol);
+    io.sockets.emit('newstock', { symbol: symbol });
+  }
+}
+
 io.on('connection', function (socket) {
   io.sockets.emit('broadcast message', { broadcast: 'msg' });
+  
+  socket.emit('allstocks', { stocks: stocksArr });
   
   socket.emit('news', { hello: 'world' });
   
   socket.on('my other event', function (data) {
     console.log(data);
   });
+  
+
+  
+  socket.on('newstock', function (data) {
+    addStock(data.symbol);
+    console.log("stocksArr=" + stocksArr);
+  });
+  
+  //socket.emit('newstock', { symbol: data.Elements[0].Symbol });
 });
 
 // listen for requests :)
